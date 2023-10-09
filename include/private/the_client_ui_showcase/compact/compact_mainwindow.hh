055fed2275a8c15038f08c83f6da98501771f73e
Ghasem Ramezani <g1999ramezani@gmail.com>
2023-10-09 22:21:05 +0330

    Setup the compact view
    Signed-off-by: Ghasem Ramezani <g1999ramezani@gmail.com>

--- include/private/the_client_ui_showcase/compact/compact_mainwindow.hh
+++ include/private/the_client_ui_showcase/compact/compact_mainwindow.hh
@@ -1,10 +1,24 @@
 #pragma once
 
+#include <QtCore/QPointer>
+#include <QtWidgets/QSplitter>
+#include <QtWidgets/QTreeWidget>
 #include <QtWidgets/QWidget>
 
 class CompactMainWindow : public QWidget {
   Q_OBJECT
 
  public:
-  CompactMainWindow();
+  CompactMainWindow(QWidget* const parent = nullptr);
+
+ private:
+  void generateView();
+  void generateLayout();
+  void generateSplitter();
+  void generateTreeWidget();
+  void generateMainView();
+
+  QPointer<QSplitter> splitter_ = nullptr;
+  QPointer<QTreeWidget> tree_widget_ = nullptr;
+  QPointer<QTreeWidget> main_view_ = nullptr;
 };
--- src/compact/compact_mainwindow.cc
+++ src/compact/compact_mainwindow.cc
@@ -1,3 +1,40 @@
 #include <the_client_ui_showcase/compact/compact_mainwindow.hh>
+//
+#include <QtWidgets/QLayout>
+//
+#include <cassert>
 
-CompactMainWindow::CompactMainWindow() {}
+CompactMainWindow::CompactMainWindow(QWidget* const parent) : QWidget(parent) {
+  generateView();
+}
+
+void CompactMainWindow::generateView() {
+  generateLayout();
+  generateSplitter();
+  generateTreeWidget();
+  generateMainView();
+}
+void CompactMainWindow::generateLayout() {
+  QPointer<QVBoxLayout> const layout = new QVBoxLayout;
+  this->QWidget::setLayout(layout);
+}
+void CompactMainWindow::generateSplitter() {
+  assert(this->QWidget::layout() != nullptr);
+
+  splitter_ = new QSplitter;
+  this->QWidget::layout()->addWidget(splitter_);
+}
+void CompactMainWindow::generateTreeWidget() {
+  assert(splitter_ != nullptr);
+
+  tree_widget_ = new QTreeWidget;
+  splitter_->addWidget(tree_widget_);
+  splitter_->setStretchFactor(0, 30);
+}
+void CompactMainWindow::generateMainView() {
+  assert(splitter_ != nullptr);
+
+  main_view_ = new QTreeWidget;
+  splitter_->addWidget(main_view_);
+  splitter_->setStretchFactor(1, 100);
+}
